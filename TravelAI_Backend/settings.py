b587493e98df3c075a370e5a89cc654e3b8d0743
mrmuke <m.xingster@gmail.com>
2021-05-27 15:39:17 -0500

    deploy

--- TravelAI_Backend/settings.py
+++ TravelAI_Backend/settings.py
@@ -93,13 +93,13 @@ WSGI_APPLICATION = 'TravelAI_Backend.wsgi.application'
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
-
-DATABASES['default']['ENGINE']='django.db.backends.sqlite3'
-DATABASES['default']['NAME'] = 'db.sqlite3'
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+#DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- TravelAI_Backend/urls.py
+++ TravelAI_Backend/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/cities/', include('cities.urls') ),
     path('api/plan/', include('itinerary.urls') ),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+[re_path('.*', index = never_cache(TemplateView.as_view(template_name='index.html')))]
